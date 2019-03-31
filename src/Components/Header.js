d6c344f97b002f095b5510d826feaf104d42d132
Mark <Mark@Marks-Air-3.nyc.rr.com>
2019-03-31 11:29:54 -0400

    Revert to 09057b6

--- src/Components/Header.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import "./App.css";
-
-class Header extends Component {
-  return() {
-    return <div> What the heck does it look like </div>;
-  }
-}
---
+++ src/components/pages/homePage.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render() {
+    return (
+        <div id = "container">
+        
+            <div id = "home">
+        
+                <h1>Welcome To The Handball Court Locator</h1>
+
+                <p>Use this site to to find a park with a handball court near your location. Explore different areas of NYC and discover new parks.</p>
+
+                <div className = "startButton">
+
+                START BUTTON
+
+                </div>
+        
+            </div>
+        
+        </div>
+    );
+  }
+}
+export default Homepage;
