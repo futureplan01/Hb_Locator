1b79732fb86788a0b0192120fb498852cfcae8a9
Mark <Mark@Marks-Air-3.nyc.rr.com>
2019-03-31 11:03:39 -0400

    Revert to commit 848df85

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
-
-//COMPONENTS
-import Header from './components/headerComponent/header';
-import Homepage from 
-'./components/pages/homePage';
-
-//CSS SHEET
-import './assets/css/style.css';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-        
-        <div id = "appContainer">
-        
-            <Header/>
-                
-                <Homepage/>
-        
-        
-        
-        </div>
-        
+    <div>    
+     <h1>Header</h1>
+    <h2>Some other header</h2>
+    </div>    
     );
   }
 }
