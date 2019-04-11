035e4f74093e1c07ffb8cf0a6d4019a686852553
Mark <Mark@Marks-Air-3.nyc.rr.com>
2019-04-11 10:29:11 -0400

    flexbox view, layout change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 //COMPONENTS
+<<<<<<< HEAD
 import Main from "./components/mainComponent/main";
+=======
+import Main from "./components/MainComponent";
+import MapContainer from "./components/MapContainer";
+>>>>>>> caea5e00083fc2ed627d40d4de98efdb5a0ff655
 
 //CSS SHEET
 import "./assets/css/style.css";
@@ -10,15 +15,11 @@ import "./assets/css/style.css";
 class App extends Component {
   render() {
     return (
-        <div id = "appContainer">
-        
-          <Switch>
-            <Route exact path="/" component={Main} />
-            <Route exact path="/map" component={Main} />
-            <Route exact path="/anything_else" component={Main} />
-          </Switch>
-        
-        </div>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/map" component={MapContainer} />
+        <Route exact path="/anything_else" component={Main} />
+      </Switch>
     );
   }
 }
