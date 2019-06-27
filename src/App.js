bac9cd0002fe33a0d94b00ce306fdac4174218d6
Jesaiah Prayor <JPrayor@dcas.nyc.gov>
2019-06-27 15:28:18 -0400

    Refractoring

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from "react-router-dom";
 //COMPONENTS 
 import Main from "./components/main";
 import MapContainer from "./components/MapComponents/MapContainer";
-import Handball from "./components/MapComponents/RecomposeMap";
+import NewMap from "./components/MapComponents/RecomposeMap";
 
 //CSS SHEET
 import "./assets/css/style.css";
@@ -14,8 +14,8 @@ class App extends Component {
     return (
       <Switch>
         <Route exact path="/" component={Main} />
-        <Route exact path="/map" component={MapContainer} />
-        <Route exact path="/NewMap" component={Handball} />
+        <Route exact path="/map" component={NewMap} />
+        <Route exact path="/NewMap" component={MapContainer} />
       </Switch>
     );
   }
